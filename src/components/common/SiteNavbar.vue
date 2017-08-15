c8741a610d0fb6934369de094def1e5469878229
Stephen Ward <srmward@gmail.com>
2017-08-14 20:06:11 -0700

    fixing slight nav issue

--- src/components/common/SiteNavbar.vue
+++ src/components/common/SiteNavbar.vue
@@ -9,8 +9,8 @@
     <ul class="nav navbar-nav">
       <li><router-link v-bind:to="'/about'">Why Polar Shades?</router-link></li>
       <li v-show="!retailRoute"><router-link v-bind:to="'/retail'">Retail</router-link></li>
-      <li v-show="retailRoute"><router-link v-bind:to="'retail/roller-shades'">Roller Shades</router-link></li>
       <li v-show="retailRoute"><router-link v-bind:to="'/retail/awnings'">Awnings</router-link></li>
+      <li v-show="retailRoute"><router-link v-bind:to="'/retail/roller-shades'">Roller Shades</router-link></li>
       <li v-show="retailRoute"><router-link v-bind:to="'/retail/shutters'">Shutters</router-link></li>
       <li v-show="retailRoute"><router-link v-bind:to="'/retail/draperies-and-blinds'">Draperies &amp; Blinds</router-link></li>
       <li v-show="!retailRoute"><a href="//www.polarshade.com">Business</a></li>
