d3e0f78567ae9bb079b140e73698f98d06743c39
Stephen Ward <srmward@gmail.com>
2017-09-23 10:29:26 -0700

    deploying build to handle static serving of 404 refresh

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback');
 app = express();
 app.use(serveStatic(__dirname));
-app.use(history());
+app.use(history({
+  index: 'dist/index.html'
+}));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
